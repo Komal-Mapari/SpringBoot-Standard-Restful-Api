93396037c921adf09cbf772661a512f4c835b43d
priya7121 <priyaamale12@gmail.com>
2024-11-18 15:12:11 +0530

    i variable added

--- src/test/java/com/cjc/app/ApplicationTests.java
+++ src/test/java/com/cjc/app/ApplicationTests.java
@@ -2,9 +2,11 @@ package com.cjc.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+ 
 @SpringBootTest
 class ApplicationTests {
+
+private int i;
 
 	@Test
 	void contextLoads() {
