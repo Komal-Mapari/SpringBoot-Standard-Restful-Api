e7bad69c683f20c441bd8d0cd6bbfffe699233d5
priya7121 <priyaamale12@gmail.com>
2024-11-18 15:33:44 +0530

    add i 50

--- src/main/java/com/cjc/app/Application.java
+++ src/main/java/com/cjc/app/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+private int i=50;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
